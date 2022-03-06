33087942a1c21724b949bee566c9d970752438ce
kvrandrup <kristina.randrup@gmail.com>
2022-03-05 17:44:54 -0800

    ughhhhhhh

--- js/main.js
+++ js/main.js
@@ -22,32 +22,37 @@ function createMap(){
     }).addTo(mymap);
 
     //add WA boundary to map
-    WAgetData(mymap);
-
-    //add King County boundary to map
-    KinggetData(mymap);
-};
-
-
-//function to get WA boundary and place it on the map
-function WAgetData(maymap){
     $.ajax("data/wa_boundary.geojson", {
 	  dataType: "json", 
 	  success: function(response) {
 		L.geoJson(response, {style: WAstyle}).addTo(mymap);
 	  }
     });
-}
+
+    //add King County boundary to map
+    //KinggetData(mymap);
+};
+
+
+//function to get WA boundary and place it on the map
+//function WAgetData(maymap){
+//    $.ajax("data/wa_boundary.geojson", {
+//	  dataType: "json", 
+//	  success: function(response) {
+//		L.geoJson(response, {style: WAstyle}).addTo(mymap);
+//	  }
+//   });
+//}
 
 //function to get King County boundary and place it on the map
-function KinggetData(maymap){
-    $.ajax("data/king_county.geojson", {
-	  dataType: "json", 
-	  success: function(response) {
-		L.geoJson(response, {style: Kingstyle}).addTo(mymap);
-	  }
-    });
-}
+//function KinggetData(maymap){
+//    $.ajax("data/king_county.geojson", {
+//	  dataType: "json", 
+//	  success: function(response) {
+//		L.geoJson(response, {style: Kingstyle}).addTo(mymap);
+//	  }
+//    });
+//}
 
 function WAstyle(feature) {
     return {
